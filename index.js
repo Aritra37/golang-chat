1a8a983d07a7bdae31285ec68ec64d3f04baed42
Aritra Muhuri <93440930+Aritra37@users.noreply.github.com>
2024-07-01 20:28:20 +0530

    Added other react files

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-// api/index.js
-var socket = new WebSocket('ws://localhost:9000/ws');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-let connect = (cb) => {
-  console.log("connecting")
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-  socket.onopen = () => {
-    console.log("Successfully Connected");
-  }
-  
-  socket.onmessage = (msg) => {
-    console.log("Message from WebSocket: ", msg);
-    cb(msg);
-  }
-
-  socket.onclose = (event) => {
-    console.log("Socket Closed Connection: ", event)
-  }
-
-  socket.onerror = (error) => {
-    console.log("Socket Error: ", error)
-  }
-};
-
-let sendMsg = (msg) => {
-  console.log("sending msg: ", msg);
-  socket.send(msg);
-};
-
-export { connect, sendMsg };
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
